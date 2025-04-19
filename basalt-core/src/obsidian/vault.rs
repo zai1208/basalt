3916185bf946dc6ff8af3efee02526ae3175fff5
Erik Kinnunen <erik.kinn@gmail.com>
2025-04-19 22:09:15 +0300

    Move `Default` impl of `Note` under `note.rs`
    By mistake the Default impl was defined under `vault.rs` and in this
    commit was changed to correct place. The default for `SystemTime` was
    changed from now to unix epoch, which makes this commit breaking.
    
    Changelog: breaking
    Module: basalt-core
    Scope: obsidian

--- basalt-core/src/obsidian/vault.rs
+++ basalt-core/src/obsidian/vault.rs
@@ -3,7 +3,6 @@ use std::{
     fs::{self, read_dir},
     path::{Path, PathBuf},
     result,
-    time::SystemTime,
 };
 
 use serde::{Deserialize, Deserializer};
@@ -100,26 +99,15 @@ impl<'de> Deserialize<'de> for Vault {
                     .to_string();
                 Ok(Vault {
                     name,
-                    path: self.path,
-                    open: self.open.unwrap_or_default(),
-                    ts: self.ts,
+                    path: value.path,
+                    open: value.open.unwrap_or_default(),
+                    ts: value.ts,
                 })
             }
         }
 
-        Json::from(Deserialize::deserialize(deserializer)?)
-            .try_into()
-            .map_err(serde::de::Error::custom)
-    }
-}
-
-impl Default for Note {
-    fn default() -> Self {
-        Self {
-            name: String::default(),
-            path: PathBuf::default(),
-            created: SystemTime::now(),
-        }
+        let deserialized: Json = Deserialize::deserialize(deserializer)?;
+        deserialized.try_into().map_err(serde::de::Error::custom)
     }
 }
 
