bd615f8da8813312fd9351b1ccdcf5e29b164d6d
Erik Kinnunen <erik.kinn@gmail.com>
2025-04-29 23:24:39 +0300

    Add `app` module
    App module is the main basalt app. The app holds the complete state of
    the application and is built with ELM architecture, which means that the
    view and the state management is separated. The state updates are
    controlled by events called actions and are listed in the `Action` enum.
    
    The basalt app has two screens Start and Main.
    
    The start screen displays the application 'Splash' screen and the Main
    screen as the name states is the screen with the main functionality of
    the app like note selection and viewing.
    
    The keybindings are also setup in the app module inside the
    `handle_press_key_event` function.
    
    Changelog: added
    Module: basalt

--- basalt/src/main.rs
+++ basalt/src/main.rs
@@ -1,5 +1,9 @@
 use std::io;
 
+use app::App;
+use basalt_core::obsidian::ObsidianConfig;
+
+pub mod app;
 pub mod help_modal;
 pub mod sidepanel;
 pub mod start;
@@ -9,5 +13,12 @@ pub mod vault_selector;
 pub mod vault_selector_modal;
 
 fn main() -> io::Result<()> {
+    let terminal = ratatui::init();
+    let obsidian_config = ObsidianConfig::load().unwrap();
+    let vaults = obsidian_config.vaults();
+
+    App::start(terminal, vaults)?;
+    ratatui::restore();
+
     Ok(())
 }
